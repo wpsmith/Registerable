a6532ac43aa225fa8b867c8874f62a6d14cde674
wpsmith <t@wpsmith.net>
2020-06-08 10:26:45 -0400

    Fix add_action; update PHPDocs

--- src/Registerable.php
+++ src/Registerable.php
@@ -10,7 +10,7 @@
  *
  * @package    WPS\Registerable
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2019 Travis Smith
+ * @copyright  2015-2020 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
@@ -20,6 +20,7 @@
 namespace WPS\WP;
 
 use WPS;
+use function add_action;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
